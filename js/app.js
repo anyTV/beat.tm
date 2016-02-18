736e4f216e7d8cd26c62a192d78788e422ae56b5
Ryan <castaneda.ry@gmail.com>
2016-02-18 17:58:01 +0800

    added languages on bottom page

--- js/app.js
+++ js/app.js
@@ -2,32 +2,35 @@
     var app = angular.module('beat', ['ngCookies', 'pascalprecht.translate']);
 
     app.config(['$translateProvider', function ($translateProvider) {
-      // // configures staticFilesLoader
-      // $translateProvider.useStaticFilesLoader({
-      //   prefix: 'data/locale-',
-      //   suffix: '.json'
-      // });
-      // load 'en' table on startup
-      // $translateProvider.useUrlLoader('foo/zh.json');
       $translateProvider.useStaticFilesLoader({
           prefix: '/locale/',
           suffix: '.json'
       });
 
       $translateProvider.determinePreferredLanguage();
-
       $translateProvider.useLocalStorage();
-      // $translateProvider.preferredLanguage('zh');
-      // $translateProvider.preferredLanguage('en');
     }]);
 
     app.controller('homeController', ['$translate', '$scope', function ($translate, $scope) {
         $scope.changeLanguage = function (langKey) {
-            console.log("langKey", langKey);
             $translate.use(langKey);
         };
 
-        console.log("$translate.use()", $translate.proposedLanguage());
+        $scope.languages = [
+            {
+              key: 'en_US',
+              text: 'English'
+            },
+            {
+              key: 'zh',
+              text: '中文(简体)'
+            },
+            {
+              key: 'pt_PT',
+              text: 'Português'
+            }
+        ];
+
     }]);
 
 })();
